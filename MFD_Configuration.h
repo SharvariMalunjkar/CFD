329347a125652ee87068ec30ab50e83b16f03c0a
SharvariMalunjkar <malunjkar.sharvari@gmail.com>
2016-04-04 02:00:12 -0600

    header file added

---
+++ MFD_Configuration.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<getopt.h>
+
+#include"../common/Configuration.h"
+
+/**************************************************************
+ *
+ * Configuration class for MFD_Configuration
+ *
+ * ***************************************************************/
+
+class MFD_Configuration:public Configuration{
+
+public:  
+
+    MFD_Configuration(int argc,char* argv[]);
+
+
+}; 
